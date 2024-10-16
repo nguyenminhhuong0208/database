c8d19b11d71243af3773f740c2c596f0b6d7d8fe
Final_project_22022542_Nguyen_Minh_Huong <nguyenminhhuong1222@gmail.com>
2024-10-17 00:10:43 +0700

    add home page

---
+++ home.php
@@ -0,0 +1,23 @@
+<?php
+include 'components/connection.php';
+?>
+
+<style type="text/css">
+<?php include 'style.css'; ?>
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel='stylesheet'>
+    <title>Green Coffee - home page</title>
+</head>
+<body>
+    <?php include 'components/header.php'; ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="script.js"></script>
+    <?php include 'components/alert.php'; ?>
+</body>
+</html>
