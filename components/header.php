4f45d1262b284f470cc16a48d7fc00a13086399b
kiwikiwi <144357652+Tuyen0712@users.noreply.github.com>
2024-10-26 17:02:54 +0700

    fix

--- components/header.php
+++ components/header.php
@@ -11,18 +11,18 @@
         <div class="icons">
             <i class="bx bxs-user" id="user-btn"></i>
             <?php 
-                $count_wishlist_items = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id=?");
+                $count_wishlist_items = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id = ?");
                 $count_wishlist_items->execute([$user_id]);
                 $total_wishlist_items = $count_wishlist_items->rowCount();
             ?>
-            <a href="wishlist.php" class="cart-btn"><i class="bx bx-heart"></i><sup><?=$total_wishlist_items?></sup></a>
+            <a href="wishlist.php" class="cart-btn"><i class="bx bx-heart"></i><sup ><?=$total_wishlist_items?></sup></a>
             <?php
                 $count_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id=?");
                 $count_cart_items->execute([$user_id]);
                 $total_cart_items = $count_cart_items->rowCount();
             ?>
             <a href="cart.php" class="cart-btn">
-                <i class="bx bx-cart-download"></i><sup><?=$total_cart_items?></sup>
+                <i class="bx bx-cart-download"></i><sup ><?=$total_cart_items?></sup>
             </a>
             <i class="bx bx-list-plus" id="menu-btn" style="font-size: 2rem;"></i>
         </div>
