fea5dfd11f94c366e099e96327e6d2fda2c0b531
thuong <thuongthuong051103@gmail.com>
2024-10-28 14:09:52 +0700

    upload cart.php

--- components/alert.php
+++ components/alert.php
@@ -1,26 +1,45 @@
 <?php
 
     if(isset($success_msg)){
-        foreach($success_msg as $success_msg){
-            echo'<script>swal("'.$success_msg.'", "","success");</script>';
+        // Kiểm tra nếu là mảng thì duyệt qua từng phần tử
+        if (is_array($success_msg)) {
+            foreach($success_msg as $msg){
+                echo '<script>swal("'.$msg.'", "", "success");</script>';
+            }
+        } else {
+            // Nếu không phải mảng, hiển thị trực tiếp
+            echo '<script>swal("'.$success_msg.'", "", "success");</script>';
         }
     }
 
     if(isset($warning_msg)){
-        foreach($warning_msg as $warning_msg){
-            echo'<script>swal("'.$warning_msg.'", "","warning");</script>';
+        if (is_array($warning_msg)) {
+            foreach($warning_msg as $msg){
+                echo '<script>swal("'.$msg.'", "", "warning");</script>';
+            }
+        } else {
+            echo '<script>swal("'.$warning_msg.'", "", "warning");</script>';
         }
     }
 
     if(isset($info_msg)){
-        foreach($info_msg as $info_msg){
-            echo'<script>swal("'.$info_msg.'", "","info");</script>';
+        if (is_array($info_msg)) {
+            foreach($info_msg as $msg){
+                echo '<script>swal("'.$msg.'", "", "info");</script>';
+            }
+        } else {
+            echo '<script>swal("'.$info_msg.'", "", "info");</script>';
         }
     }
 
     if(isset($error_msg)){
-        foreach($error_msg as $error_msg){
-            echo'<script>swal("'.$error_msg.'", "","error");</script>';
+        if (is_array($error_msg)) {
+            foreach($error_msg as $msg){
+                echo '<script>swal("'.$msg.'", "", "error");</script>';
+            }
+        } else {
+            echo '<script>swal("'.$error_msg.'", "", "error");</script>';
         }
     }
+
 ?>
