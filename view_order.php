cc63020c7e1a476fad1437263142c54d2084b9fc
HoaiThuong <thuongthuong051103@gmail.com>
2024-11-29 18:55:38 +0700

    add order-view and fix interface

--- view_order.php
+++ view_order.php
@@ -3,7 +3,7 @@
     session_start();
     if(isset($_SESSION['user_id'])) {
         $user_id = $_SESSION['user_id'];
-    }else{
+    } else {
         $user_id = '';
     }
 
@@ -11,16 +11,26 @@
         session_destroy();
         header("location: login.php");
     }
+
     if (isset($_GET['get_id'])) {
         $get_id = $_GET['get_id'];
-      } else {
+    } else {
         $get_id = '';
         header('location: order.php');
     }
+
+    if (isset($_POST['cancel'])) {
+        $update_order = $conn->prepare("UPDATE orders SET status = ? WHERE id = ?");
+        $update_order->execute(['canceled', $get_id]);
+        header("Location: order.php"); 
+        exit();
+    }
 ?>
+
 <style type="text/css">
     <?php include 'style.css'; ?>
 </style>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,22 +43,19 @@
     <?php include 'components/header.php'; ?>
     <div class="main">
         <div class="banner">
-            <h1>order detail</h1>
+            <h1>Order Detail</h1>
         </div>
         <div class="title2">
-            <a href="home.php">home</a><span>\ order detail</span>
+            <a href="home.php">Home</a><span>\ Order Detail</span>
         </div>
         <section class="order-detail">
             <div class="box-container">
                 <div class="title">
-                    <image src="img/download.png" class="logo">
-                    <h1>order detail</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum deserunt
-                        minus variant
-                        tenetur
-                    </p>
+                    <img src="img/download.png" class="logo">
+                    <h1>Order Detail</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolorum deserunt minus variant tenetur.</p>
                 </div>
-                <div class = "box-container">
+                <div class="box-container">
                     <?php
                         $grand_total = 0;
                         $select_orders = $conn->prepare("SELECT * FROM orders WHERE id=? LIMIT 1");
@@ -62,35 +69,40 @@
                                 if ($select_product->rowCount() > 0) {
                                     while ($fetch_product = $select_product->fetch(PDO::FETCH_ASSOC)) {
                                         $sub_total = ($fetch_order['price'] * $fetch_order['qty']);
-                                        $grand_total += $sub_total; 
-                                    }
-                                }
-                            }
-                        }
+                                        $grand_total += $sub_total;
                     ?>
                     <div class="box">
                         <div class="col">
-                            <p class="title" title="Ngày" class="bi bi-calendar-fill"><?= $fetch_order['date']; ?></p>
+                            <p class="title"><?= $fetch_order['date']; ?></p>
                             <img src="image/<?= $fetch_product['image']; ?>" class="image">
                             <p class="price"><?= $fetch_product['price']; ?> x <?= $fetch_order['qty']; ?></p>
                             <h3 class="name"><?= $fetch_product['name']; ?></h3>
                             <p class="grand-total">Total amount payable: <span>$<?= $grand_total; ?></span></p>
                         </div> 
                         <div class="col">
-
+                            <p class="title">Status</p>
+                            <p class="status" style="color:<?php if($fetch_order['status']=='delivered'){echo 'green';}elseif($fetch_order['status']=='canceled') {echo 'red';}else{echo 'orange';} ?>"><?= $fetch_order['status'] ?></p>
+                            <?php if ($fetch_order['status'] == 'canceled') { ?>
+                                <a href="checkout.php?get_id=<?= $fetch_product['id']; ?>" class="btn">Order Again</a>
+                            <?php } else { ?>
+                                <form method="post">
+                                    <button type="submit" name="cancel" class="btn" onclick="return confirm('Do you want to cancel this order?')">Cancel Order</button>
+                                </form>
+                            <?php } ?>
                         </div>
                     </div>
                     <?php
                                     }
                                 }
                             }
+                        } else {
+                            echo '<p class="empty">No order placed yet!</p>';
                         }
                     ?>
                 </div>
             </div>
         </section>
     </div>
-    <!-- //<?php include 'components/footer.php'; ?> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
     <script type="text/javascript" src="script.js"></script>
     <?php include 'components/alert.php'; ?>
