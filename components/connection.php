6995453c341801f21444b414a72bb28e031a21f2
kiwikiwi <144357652+Tuyen0712@users.noreply.github.com>
2024-11-30 00:07:36 +0700

    agian

--- components/connection.php
+++ components/connection.php
@@ -9,18 +9,14 @@
      echo"Can not connect to the database";
    }
 
-   if (!function_exists('unique_id')) {
-      function unique_id() {
-          $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-          $charLength = strlen($chars);
-          $randomString = '';
-  
-          for ($i = 0; $i < 20; $i++) {
-              $randomString .= $chars[random_int(0, $charLength - 1)];
-          }
-  
-          return $randomString;
+   function unique_id(){
+      $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $charLength = strlen($chars);
+      $randomString = '';
+
+      for($i = 0; $i < 20; $i++){
+         $randomString .= $chars[mt_rand(0,$charLength -1)];
       }
-  }
-  
+      return $randomString;
+   }
 ?>
