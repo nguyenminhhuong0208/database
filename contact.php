44b74e4d5240dce2ac271dc7d5f4968a194496c6
kiwikiwi <144357652+Tuyen0712@users.noreply.github.com>
2024-12-03 12:35:47 +0700

    sua stle va them about

--- contact.php
+++ contact.php
@@ -2,101 +2,122 @@
 include 'components/connection.php';
 session_start();
 if (!isset($_SESSION['user_id'])) {
-    $_SESSION['user_id'] = ''; 
+    $_SESSION['user_id'] = '';
 }
 
-if(isset($_SESSION['user_id'])) {
+if (isset($_SESSION['user_id'])) {
+    $_SESSION['user_id'] = '';
+}
+
+if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
-}else{
+} else {
     $user_id = '';
-
 }
 
-if(isset($_POST['logout'])) {
-    session_destroy();
-    header("location: login.php");
+// Message sending
+
+if (isset($_POST['submit-btn'])) {
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+    $message = $_POST['message'];
+
+    // Insert message into database 
+    $insert_message = $conn->prepare("INSERT INTO `message` (user_id, name, email, message) VALUES (?, ?, ?, ?)");
+    $insert_message->execute([$user_id, $name, $email, $message]);
+    $success_msg[] = 'Message sent successfully!';
+    // echo 'Message sent successfully';
+
+    //e xoa doan nay nua de no hien thong bao
+    //vang a
+    // Redirect with success parameter
+    //header("Location: contact.php?success=1");
+    //exit;
 }
 ?>
 
 <style type="text/css">
-<?php include 'style.css'; ?>
+    <?php include 'style.css'; ?>
 </style>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" >
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
     <title>Green Coffee - contact page</title>
 </head>
+
 <body>
     <?php include 'components/header.php'; ?>
     <div class="main">
-    <div class="banner">
-        <h1>Contact us</h1>
-    </div>
-    
+        <div class="banner">
+            <h1>Contact us</h1>
+        </div class="form-container">
+        <form action="" method="post">
+            <div class="title">
+                <img src="img/download.png" class="logo">
+                <h1>Leave a message</h1>
+            </div>
             <div class="input-field">
-                <p>Your name </p>
+                <p>Your name <sup>*<sup></p>
                 <input type="text" name="name">
             </div>
             <div class="input-field">
-                <p>Your email </p>
+                <p>Your email <sup>*<sup></p>
                 <input type="email" name="email">
             </div>
             <div class="input-field">
-                <p>Your number </p>
-                <input type="text" name="number">
-            </div>
-            <div class="input-field">
-                <p>Your message  </p>
+                <p>Your message <sup>*<sup></p>
                 <textarea name="message" id=""></textarea>
             </div>
-            <button type="submit" name="submit-btn" class="btn"> Send message</button>
-
+            <button type="submit" name="submit-btn" class="btn">Send message</button>
+            <?php if (isset($_GET['success']) && $_GET['success'] == 1): ?>
+                <!-- <p class="success-message">Message sent successfully!</p> -->
+            <?php endif; ?>
         </form>
-    
     </div>
     <div class="address">
         <div class="title">
-                <img src="img/download.png" alt="" class="logo">
-                <h1>Contact detail</h1>
+            <img src="img/download.png" alt="" class="logo">
+            <h1>Contact detail</h1>
         </div>
 
         <div class="box-container">
-            <div class="box"> 
+            <div class="box">
                 <i class="bx bxs-map-pin"></i>
                 <div>
                     <h4>Address</h4>
                     <p>Truong Dai Hoc Cong Nghe</p>
                 </div>
             </div>
-            <div class="box"> 
+            <div class="box">
                 <i class="bx bxs-phone-call"></i>
                 <div>
-                <h4>Facebook</h4>
+                    <h4>Facebook</h4>
                     <p><?php
                         echo '<a href="https://www.facebook.com/profile.php?id=100029231884618" target="_blank">Minh Huong</a>';
-                    ?></p>
+                        ?></p>
                 </div>
             </div>
-            <div class="box"> 
+            <div class="box">
                 <i class="bx bxs-phone-call"></i>
                 <div>
-                <h4>Facebook</h4>
+                    <h4>Facebook</h4>
                     <p><?php
                         echo '<a href="https://www.facebook.com/profile.php?id=100051391368220" target="_blank">Thanh Tuyen</a>';
-                    ?></p>
+                        ?></p>
                 </div>
             </div>
-            <div class="box"> 
+            <div class="box">
                 <i class="bx bxs-phone-call"></i>
                 <div>
                     <h4>Facebook</h4>
                     <p><?php
                         echo '<a href="https://www.facebook.com/hoaithuon.me" target="_blank">Hoai Thuong</a>';
-                    ?></p>
+                        ?></p>
                 </div>
             </div>
         </div>
@@ -105,4 +126,5 @@ if(isset($_POST['logout'])) {
     <script src="script.js"></script>
     <?php include 'components/alert.php'; ?>
 </body>
+
 </html>
--- view_products.php
+++ view_products.php
@@ -1,81 +1,83 @@
 <?php
-    include 'components/connection.php';
-    session_start();
-    if(isset($_SESSION['user_id'])) {
-        $user_id = $_SESSION['user_id'];
-    }else{
-        $user_id = '';
+include 'components/connection.php';
+session_start();
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+} else {
+    $user_id = '';
+}
+
+if (isset($_POST['logout'])) {
+    session_destroy();
+    header("location: login.php");
+}
+//adding products in wishlist
+if (isset($_POST['add_to_wishlist'])) {
+    $id = unique_id();
+    $product_id = $_POST['product_id'];
+
+    $varify_wishlist = $conn->prepare("SELECT * FROM wishlist WHERE user_id = ? AND product_id = ?");
+    $varify_wishlist->execute([$user_id, $product_id]);
+
+    $cart_num = $conn->prepare("SELECT * FROM cart WHERE user_id = ? AND product_id = ?");
+    $cart_num->execute([$user_id, $product_id]);
+
+    if ($varify_wishlist->rowCount() > 0) {
+        $warning_msg[] = 'product already exist in your wishlist';
+    } else if ($cart_num->rowCount() > 0) {
+        $warning_msg[] = 'product already exist in your cart';
+    } else {
+        $select_price = $conn->prepare("SELECT * FROM products WHERE id = ? LIMIT 1");
+        $select_price->execute([$product_id]);
+        $fetch_price = $select_price->fetch(PDO::FETCH_ASSOC);
+
+        $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(id,user_id,product_id,price) VALUES(?,?,?,?)");
+        $insert_wishlist->execute([$id, $user_id, $product_id, $fetch_price['price']]);
+        $success_msg[] = 'product added to wishlist successfully!';
     }
+}
+//adding products in cart
+if (isset($_POST['add_to_cart'])) {
+    $id = unique_id();
+    $product_id = $_POST['product_id'];
 
-    if(isset($_POST['logout'])) {
-        session_destroy();
-        header("location: login.php");
+    $qty = $_POST['qty'];
+    $qty = filter_var($qty, FILTER_SANITIZE_STRING);
+
+    $varify_cart = $conn->prepare("SELECT * FROM cart WHERE user_id = ? AND product_id = ?");
+    $varify_cart->execute([$user_id, $product_id]);
+
+    $max_cart_items = $conn->prepare("SELECT * FROM cart WHERE user_id = ?");
+    $max_cart_items->execute([$user_id]);
+
+    if ($varify_cart->rowCount() > 0) {
+        $warning_msg[] = 'product already exist in your wishlist';
+    } else if ($max_cart_items->rowCount() > 20) {
+        $warning_msg[] = 'cart is full';
+    } else {
+        $select_price = $conn->prepare("SELECT * FROM products WHERE id = ? LIMIT 1");
+        $select_price->execute([$product_id]);
+        $fetch_price = $select_price->fetch(PDO::FETCH_ASSOC);
+
+        $insert_cart = $conn->prepare("INSERT INTO `cart`(id,user_id,product_id,price,qty) VALUES(?,?,?,?,?)");
+        $insert_cart->execute([$id, $user_id, $product_id, $fetch_price['price'], $qty]);
+        $success_msg[] = 'product added to cart successfully!';
     }
-    //adding products in wishlist
-    if(isset($_POST['add_to_wishlist'])) {
-        $id = unique_id();
-        $product_id = $_POST['product_id'];
-
-        $varify_wishlist = $conn->prepare("SELECT * FROM wishlist WHERE user_id = ? AND product_id = ?");
-        $varify_wishlist->execute([$user_id,$product_id]);
-
-        $cart_num = $conn->prepare("SELECT * FROM cart WHERE user_id = ? AND product_id = ?");
-        $cart_num->execute([$user_id,$product_id]);
-
-        if($varify_wishlist->rowCount()>0) {
-            $warning_msg[] = 'product already exist in your wishlist';
-        } else if ($cart_num->rowCount()>0) {
-            $warning_msg[] = 'product already exist in your cart';
-        } else {
-            $select_price= $conn->prepare("SELECT * FROM products WHERE id = ? LIMIT 1");
-            $select_price->execute([$product_id]);
-            $fetch_price=$select_price->fetch(PDO::FETCH_ASSOC);
-        
-            $insert_wishlist = $conn->prepare("INSERT INTO `wishlist`(id,user_id,product_id,price) VALUES(?,?,?,?)");
-            $insert_wishlist->execute([$id,$user_id,$product_id,$fetch_price['price']]);
-            $success_msg[] = 'product added to wishlist successfully!';
-        }
-    }
-    //adding products in cart
-    if(isset($_POST['add_to_cart'])) {
-        $id = unique_id();
-        $product_id = $_POST['product_id'];
-
-        $qty = $_POST['qty'];
-        $qty = filter_var($qty,FILTER_SANITIZE_STRING);
-
-        $varify_cart = $conn->prepare("SELECT * FROM cart WHERE user_id = ? AND product_id = ?");
-        $varify_cart->execute([$user_id,$product_id]);
-
-        $max_cart_items = $conn->prepare("SELECT * FROM cart WHERE user_id = ?");
-        $max_cart_items->execute([$user_id]);
-
-        if($varify_cart->rowCount()>0) {
-            $warning_msg[] = 'product already exist in your wishlist';
-        } else if ($max_cart_items->rowCount()>20) {
-            $warning_msg[] = 'cart is full';
-        } else {
-            $select_price= $conn->prepare("SELECT * FROM products WHERE id = ? LIMIT 1");
-            $select_price->execute([$product_id]);
-            $fetch_price=$select_price->fetch(PDO::FETCH_ASSOC);
-        
-            $insert_cart = $conn->prepare("INSERT INTO `cart`(id,user_id,product_id,price,qty) VALUES(?,?,?,?,?)");
-            $insert_cart->execute([$id,$user_id,$product_id,$fetch_price['price'],$qty]);
-            $success_msg[] = 'product added to cart successfully!';
-        }
-    }
+}
 ?>
 <style type="text/css">
     <?php include 'style.css'; ?>
 </style>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css">
     <title>Green Coffee - shop page</title>
 </head>
+
 <body>
     <?php include 'components/header.php'; ?>
     <div class="main">
@@ -88,31 +90,31 @@
         <section class="products">
             <div class="box-container">
                 <?php
-                    $select_products = $conn->prepare("SELECT * FROM products");
-                    $select_products->execute();
-                    if($select_products->rowCount() > 0) {
-                        while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)) {
+                $select_products = $conn->prepare("SELECT * FROM products");
+                $select_products->execute();
+                if ($select_products->rowCount() > 0) {
+                    while ($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)) {
                 ?>
-                    <form action="" method="post" class="box">
-                        <img src="image/<?=$fetch_products['image']; ?>" class="img">
-                        <div class="button">
-                            <button type="submit" name="add_to_cart"><i class="bx bx-cart"></i></button>
-                            <button type="submit" name="add_to_wishlist"><i class="bx bx-heart"></i></button>
-                            <a href="view_page.php?pid=<?php echo $fetch_products['id'];?>" class="bx bxs-show"></a>
-                        </div>
-                        <h3 class="name"><?=$fetch_products['name'];?></h3>
-                        <input type="hidden" name="product_id" value="<?=$fetch_products['id'];?>">
-                        <div class="flex">
-                            <p class="price">Price $<?=$fetch_products['price'];?>/-</p>
-                            <input type="number" name="qty" required min="1" max="99" maxlength="2" class="qty" value="1">
-                        </div>
-                        <a href="checkout.php?get_id=<?=$fetch_products['id'];?>" class="btn">Buy now</a>
-                    </form>
+                        <form action="" method="post" class="box">
+                            <img src="image/<?= $fetch_products['image']; ?>" class="img">
+                            <div class="button">
+                                <button type="submit" name="add_to_cart"><i class="bx bx-cart"></i></button>
+                                <button type="submit" name="add_to_wishlist"><i class="bx bx-heart"></i></button>
+                                <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="bx bxs-show"></a>
+                            </div>
+                            <h3 class="name"><?= $fetch_products['name']; ?></h3>
+                            <input type="hidden" name="product_id" value="<?= $fetch_products['id']; ?>">
+                            <div class="flex">
+                                <p class="price">Price $<?= $fetch_products['price']; ?>/-</p>
+                                <input type="number" name="qty" required min="1" max="99" maxlength="2" class="qty" value="1">
+                            </div>
+                            <a href="checkout.php?get_id=<?= $fetch_products['id']; ?>" class="btn">Buy now</a>
+                        </form>
                 <?php
-                        }
-                    } else {
-                        echo '<p></p><p class="empty">No products added yet!</p> </div>';
                     }
+                } else {
+                    echo '<p></p><p class="empty">No products added yet!</p> </div>';
+                }
                 ?>
             </div>
         </section>
@@ -122,4 +124,5 @@
     <script type="text/javascript" src="script.js"></script>
     <?php include 'components/alert.php'; ?>
 </body>
+
 </html>
